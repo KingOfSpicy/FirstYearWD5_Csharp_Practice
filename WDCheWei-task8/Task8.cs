0f05133182eb89adceb8c05ccc93a405d4535905
KingOfSpicy <sangowosan@gmail.com>
2022-03-03 10:12:00 +0200

    add working files to folder.

---
+++ WDCheWei-task8/Task8.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WDCheWei_task8
+{
+    class Task8
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount("CheWei", 10000);
+            Console.WriteLine(account);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "friend paid me back");
+            Console.WriteLine(account.Balance);
+            Console.WriteLine(account.GetAccountHistory());
+
+            //try
+            //{
+            //    var invalidAccount = new BankAccount("invalid", -55);
+            //}
+            //catch (ArgumentOutOfRangeException e)
+            //{
+            //    Console.WriteLine("Exception caught creating account with negative balance");
+            //    Console.WriteLine(e.ToString());
+            //}
+            //try
+            //{
+            //    account.MakeWithdrawal(75000, DateTime.Now, "Attempt to overdraw");
+            //}
+            //catch (InvalidOperationException e)
+            //{
+            //    Console.WriteLine("Exception caught trying to overdraw");
+            //    Console.WriteLine(e.ToString());
+            //}
+
+        }
+    }
+}
